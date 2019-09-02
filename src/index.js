bfa9551f7901d4ad98e5af62400a593c71c390c9
chickenbrain <doandinhthanh@hotmail.com>
2019-09-02 16:43:55 +1000

    Create base form elements

--- src/index.js
+++ src/index.js
@@ -1,28 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
-
-import "./styles.css";
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
 
 function App() {
   return (
     <AppContainer className="App">
-      <GreetingMsg>Hello CodeSandbox</GreetingMsg>
-      <ExcerptMsg>Start editing to see some magic happen!</ExcerptMsg>
+      <Title>Employee Info</Title>
+      <form>
+        <FirstNameInput />
+        <LastNameInput />
+        <DollarInput />
+        <PercentInput />
+        <SubmitButton />
+      </form>
     </AppContainer>
   );
 }
 
+const Title = styled.h1``;
+
 const AppContainer = styled.div`
+  flex: 1;
+`;
 
-`
+const Input = styled.input`
+  border-radius: 5px;
+`;
 
-const GreetingMsg = styled.h1`
-`
+const FirstNameInput = styled(Input)``;
 
-const ExcerptMsg = styled.h2`
-  color: blue;
-`
+const LastNameInput = styled(Input)``;
+
+const DollarInput = styled(Input)``;
+
+const PercentInput = styled(Input)``;
+
+const SubmitButton = styled(Input)``;
+
+FirstNameInput.defaultProps = {
+  type: "text",
+  name: "name",
+  placeholder: "First Name"
+};
+
+LastNameInput.defaultProps = {
+  type: "text",
+  name: "name",
+  placeholder: "Last Name"
+};
+
+DollarInput.defaultProps = {
+  type: "text",
+  name: "name",
+  placeholder: "Annual Salary"
+};
+
+PercentInput.defaultProps = {
+  type: "text",
+  name: "name",
+  placeholder: "Super Rate"
+};
+
+SubmitButton.defaultProps = {
+  type: "submit",
+  value: "Generate Payslip"
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
