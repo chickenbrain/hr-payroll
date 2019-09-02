50fb70007b16eee1fce226b4d823c073bff12641
chickenbrain <doandinhthanh@hotmail.com>
2019-09-02 16:02:28 +1000

    test commit
    making sure it works!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <AppContainer className="App">
+      <GreetingMsg>Hello CodeSandbox</GreetingMsg>
+      <ExcerptMsg>Start editing to see some magic happen!</ExcerptMsg>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+
+`
+
+const GreetingMsg = styled.h1`
+`
+
+const ExcerptMsg = styled.h2`
+  color: blue;
+`
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
